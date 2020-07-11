90173e4d62748357984502c597965318436d9e5e
Gonzako <al341522@uji.es>
2020-07-11 14:49:05 +0200

    Changed hose texture, added changes to player prefab and created player system singleton

---
+++ Assets/setChainBToPlayerLogic.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2020 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class setChainBToPlayerLogic : MonoBehaviour
+{
+
+    #region PublicFields
+
+    #endregion
+
+    #region PrivateFields
+
+    #endregion
+
+    #region UnityCallBacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var chain = GetComponent<Chain>();
+        chain.B = playerSystem.instance.playerLogic.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+    
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+    #endregion
+}
