67542b441600e791f79ea69f7a08a0375228edd7
Gonzako <al341522@uji.es>
2020-07-11 12:45:52 +0200

    Moved chain plugin, created player prefab with end Point included

---
+++ Assets/Scripts/TweenRotate.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+/// 
+/// Copyright (c) 2020 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+
+public class TweenRotate : MonoBehaviour
+{
+
+    #region PublicFields
+    [SerializeField]
+    public Vector3 targetRot = Vector3.forward * 45;
+    
+
+    #endregion
+
+    #region PrivateFields
+
+    #endregion
+
+    #region UnityCallBacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+    
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    public void tweenToRotation(float time)
+    {
+        transform.DORotateQuaternion(Quaternion.Euler(targetRot), time);
+    }
+
+    #endregion
+
+
+    #region PrivateMethods
+
+    #endregion
+}
