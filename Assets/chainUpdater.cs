556e8773503ac5171b2425ee0dcca0baa55b9109
Gonzako <al341522@uji.es>
2020-07-11 13:03:43 +0200

    gave the hose some COLOUR

---
+++ Assets/chainUpdater.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// 
+/// Copyright (c) 2020 All Rights Reserved
+///
+/// <author>Gonzako</author>
+/// <co-authors>... </co-author>
+/// <summary> Monobeavior class that does something </summary>
+
+    [RequireComponent(typeof(LineRenderer))]
+public class chainUpdater : MonoBehaviour
+{
+
+    #region PublicFields
+
+    public Transform parentOfChain;
+
+    #endregion
+
+    #region PrivateFields
+
+    LineRenderer lRenderer;
+
+    #endregion
+
+    #region UnityCallBacks
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lRenderer = GetComponent<LineRenderer>();
+        lRenderer.positionCount = parentOfChain.childCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void LateUpdate()
+    {
+        var list = new List<Vector3>();
+        for (int i = 0; i < parentOfChain.childCount; i++)
+        {
+            list.Add(parentOfChain.GetChild(i).position);
+        }
+        lRenderer.SetPositions(list.ToArray());
+    }
+
+    void OnEnable()
+    {
+    
+    }
+
+    #endregion
+
+    #region PublicMethods
+
+    #endregion
+
+
+    #region PrivateMethods
+
+    #endregion
+}
